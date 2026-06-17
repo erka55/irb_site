a6ad1086f9fb1436015e2552f87ea38845b0de53
Erdene Altan <erka@must.edu.mn>
2026-06-17 21:27:31 +0800

    Phase 0: bootstrap and custom user foundation

---
+++ backend/apps/users/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from .managers import UserManager
+
+
+class User(AbstractUser):
+
+    username = None
+
+    email = models.EmailField(
+        unique=True
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
