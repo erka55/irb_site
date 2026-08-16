9504a21410636a20701ee4f772e2695afde653c3
Erdene Altan <erka@must.edu.mn>
2026-08-17 01:28:53 +0800

    refactor(events): centralize handler registration

--- backend/apps/reviews/apps.py
+++ backend/apps/reviews/apps.py
@@ -4,10 +4,3 @@ from django.apps import AppConfig
 class ReviewsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.reviews"
-
-    def ready(self):
-        from common.events.bootstrap import (
-            register_event_handlers,
-        )
-
-        register_event_handlers()
